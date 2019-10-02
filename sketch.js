ec35e4663f1fe11fce4203ff225af111ac42e54e
kctang97 <42600194+kctang97@users.noreply.github.com>
2019-10-01 19:03:06 -0700

    drawingmachine
    new

--- sketch.js
+++ sketch.js
@@ -1,9 +1,15 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
+  background(255);
+  strokeWeight(5);
 }
 
 function draw() {
 
+  if (mouseIsPressed){
+    stroke(random(200, 255),random(200, 255),random(200, 255));
+    line(windowWidth - mouseX, windowHeight - mouseY, windowWidth - pmouseX, windowHeight - pmouseY);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+  }
 
 }

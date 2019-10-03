cd8160b4a7b48ee42bb1e0abaafbf9b02e003def
kctang97 <42600194+kctang97@users.noreply.github.com>
2019-10-02 21:33:30 -0700

    save image
    new

--- sketch.js
+++ sketch.js
@@ -1,15 +1,24 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(255);
+  background(100, 150 ,255);
   strokeWeight(5);
 }
 
 function draw() {
 
   if (mouseIsPressed){
-    stroke(random(200, 255),random(200, 255),random(200, 255));
+    stroke(map(mouseX, 0, windowWidth, 0 , windowHeight, true))
+    // stroke(random(220, 255),random(200, 255),random(220,  255));
     line(windowWidth - mouseX, windowHeight - mouseY, windowWidth - pmouseX, windowHeight - pmouseY);
     line(mouseX, mouseY, pmouseX, pmouseY);
   }
 
+}
+
+function keyTyped() {
+  // console.log('key ${key} is being typed');
+  if (key === 's'){
+    saveCanvas('fileName','png');
+  }
+  return false;
 }

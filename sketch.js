2eb213c29193a9cd57a50b17157373fd4d781c7a
kctang97 <42600194+kctang97@users.noreply.github.com>
2019-10-09 19:54:34 -0700

    update
    grid

--- sketch.js
+++ sketch.js
@@ -36,29 +36,29 @@ function keyTyped() {
     clear();
     grid();
     ary = [];
+
   }
   return false;
 }
 
 function grid() {
-  numCells = 50;
+  numCells = 60;
   fillColor = 255;
   strokeWeight(0);
-
   let x;
   let y;
 
-  for (let i = 0; i <= numCells; i += 1) {
+  for (let i = 0; i <= windowWidth; i += windowWidth / numCells) {
     gridAry[i] = [];
-    for (let j = 0; j <= numCells; j += 1) {
-      x = i * windowWidth / numCells;
-      y = j * windowHeight / numCells;
+    for (let j = 0; j <= windowHeight; j += windowHeight / numCells) {
+      x = i * width / numCells;
+      y = j * height / numCells;
       if (fillColor === 255) {
         fillColor = 200;
         gridAry[i][j] = 200;
       } else {
         fillColor = 255;
-        gridAry[i][j];
+        gridAry[i][j] = 255;;
       }
       fill(fillColor);
       rect(i, j, width / numCells, height / numCells);
